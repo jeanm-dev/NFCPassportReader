d0e83b48b6a6217c0b4ede876d04154a8ba0ead4
Jean Mouton <jeanm@bbdsoftware.com>
2022-05-06 14:34:43 +0200

    Introduced `Passport` constants to provide clarity (more context) for the element that is stored

--- Sources/NFCPassportReader/DataGroups/DataGroup1.swift
+++ Sources/NFCPassportReader/DataGroups/DataGroup1.swift
@@ -22,24 +22,44 @@ public enum DocTypeEnum: String {
 @available(iOS 13, macOS 10.15, *)
 public class DataGroup1 : DataGroup {
     
+    private enum Passport {
+        static let documentType = "5F03"
+        static let personalNumber = "53"
+        static let personalNumberCheckDigit = "5F02"
+        static let documentNumber = "5A"
+        static let documentNumberCheckDigit = "5F04"
+        static let issuingAuthority = "5F28"
+        static let documentExpiryDate = "59"
+        static let documentExpiryDateCheckDigit = "5F06"
+        static let dateOfBirth = "5F57"
+        static let dateOfBirthCheckDigit = "5F05"
+        static let gender = "5F35"
+        static let nationality = "5F2C"
+        static let firstName = "5B"
+        static let lastName = "5B"
+        static let names = "5B"
+        static let mrz = "5F1F"
+        static let mrzLineCheckDigit = "5F07"
+    }
+    
     // MARK: - Document Data - Elements
-    public private(set) lazy var documentType: String = { return String(elements["5F03"]?.first ?? "?") }()
-    public private(set) lazy var documentSubType: String = { return String(elements["5F03"]?.last ?? "?")}()
-    public private(set) lazy var personalNumber: String = { return (elements["53"] ?? "?").replacingOccurrences(of: "<", with: "" ) }()
-    public private(set) lazy var documentNumber: String = { return (elements["5A"] ?? "?").replacingOccurrences(of: "<", with: "" ) }()
-    public private(set) lazy var issuingAuthority: String = { return elements["5F28"] ?? "?" }()
-    public private(set) lazy var documentExpiryDate: String = { return elements["59"] ?? "?" }()
-    public private(set) lazy var dateOfBirth: String = { return elements["5F57"] ?? "?" }()
-    public private(set) lazy var gender: String = { return elements["5F35"] ?? "?" }()
-    public private(set) lazy var nationality: String = { return elements["5F2C"] ?? "?" }()
+    public private(set) lazy var documentType: String = { return String(elements[Passport.documentType]?.first ?? "?") }()
+    public private(set) lazy var documentSubType: String = { return String(elements[Passport.documentType]?.last ?? "?")}()
+    public private(set) lazy var personalNumber: String = { return (elements[Passport.personalNumber] ?? "?").replacingOccurrences(of: "<", with: "" ) }()
+    public private(set) lazy var documentNumber: String = { return (elements[Passport.documentNumber] ?? "?").replacingOccurrences(of: "<", with: "" ) }()
+    public private(set) lazy var issuingAuthority: String = { return elements[Passport.issuingAuthority] ?? "?" }()
+    public private(set) lazy var documentExpiryDate: String = { return elements[Passport.documentExpiryDate] ?? "?" }()
+    public private(set) lazy var dateOfBirth: String = { return elements[Passport.dateOfBirth] ?? "?" }()
+    public private(set) lazy var gender: String = { return elements[Passport.gender] ?? "?" }()
+    public private(set) lazy var nationality: String = { return elements[Passport.nationality] ?? "?" }()
     
     public private(set) lazy var lastName: String = {
-        let names = (elements["5B"] ?? "?").components(separatedBy: "<<")
+        let names = (elements[Passport.lastName] ?? "?").components(separatedBy: "<<")
         return names[0].replacingOccurrences(of: "<", with: " " )
     }()
     
     public private(set) lazy var firstName: String = {
-        let names = (elements["5B"] ?? "?").components(separatedBy: "<<")
+        let names = (elements[Passport.firstName] ?? "?").components(separatedBy: "<<")
         var name = ""
         for i in 1 ..< names.count {
             let fn = names[i].replacingOccurrences(of: "<", with: " " ).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
@@ -48,14 +68,14 @@ public class DataGroup1 : DataGroup {
         return name.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     }()
     
-    public private(set) lazy var names: [String] = { return (elements["5B"] ?? "?").components(separatedBy: "<<") }()
+    public private(set) lazy var names: [String] = { return (elements[Passport.names] ?? "?").components(separatedBy: "<<") }()
     
-    public private(set) lazy var passportMRZ: String? = { return elements["5F1F"] }()
+    public private(set) lazy var passportMRZ: String? = { return elements[Passport.mrz] }()
     
     private var elements: [String:String] = [:]
     public private(set) var isDrivers: Bool = false
     
-    required init(_ data : [UInt8]) throws {
+    required init(_ data: [UInt8]) throws {
         try super.init(data)
         datagroupType = .DG1
     }
@@ -91,57 +111,57 @@ public class DataGroup1 : DataGroup {
         }
         
         // Store MRZ data
-        elements["5F1F"] = String(bytes: body, encoding:.utf8)
+        elements[Passport.mrz] = String(bytes: body, encoding:.utf8)
     }
     
     func parseTd1(_ data : [UInt8]) {
-        elements["5F03"] = String(bytes: data[0..<2], encoding:.utf8)
-        elements["5F28"] = String( bytes:data[2..<5], encoding:.utf8)
-        elements["5A"] = String( bytes:data[5..<14], encoding:.utf8)
-        elements["5F04"] = String( bytes:data[14..<15], encoding:.utf8)
-        elements["53"] = (String( bytes:data[15..<30], encoding:.utf8) ?? "") +
+        elements[Passport.documentType] = String(bytes: data[0..<2], encoding:.utf8)
+        elements[Passport.issuingAuthority] = String( bytes:data[2..<5], encoding:.utf8)
+        elements[Passport.documentNumber] = String( bytes:data[5..<14], encoding:.utf8)
+        elements[Passport.documentNumberCheckDigit] = String( bytes:data[14..<15], encoding:.utf8)
+        elements[Passport.personalNumber] = (String( bytes:data[15..<30], encoding:.utf8) ?? "") +
             (String( bytes:data[48..<59], encoding:.utf8) ?? "")
-        elements["5F57"] = String( bytes:data[30..<36], encoding:.utf8)
-        elements["5F05"] = String( bytes:data[36..<37], encoding:.utf8)
-        elements["5F35"] = String( bytes:data[37..<38], encoding:.utf8)
-        elements["59"] = String( bytes:data[38..<44], encoding:.utf8)
-        elements["5F06"] = String( bytes:data[44..<45], encoding:.utf8)
-        elements["5F2C"] = String( bytes:data[45..<48], encoding:.utf8)
-        elements["5F07"] = String( bytes:data[59..<60], encoding:.utf8)
-        elements["5B"] = String( bytes:data[60...], encoding:.utf8)
+        elements[Passport.dateOfBirth] = String( bytes:data[30..<36], encoding:.utf8)
+        elements[Passport.dateOfBirthCheckDigit] = String( bytes:data[36..<37], encoding:.utf8)
+        elements[Passport.gender] = String( bytes:data[37..<38], encoding:.utf8)
+        elements[Passport.documentExpiryDate] = String( bytes:data[38..<44], encoding:.utf8)
+        elements[Passport.documentExpiryDateCheckDigit] = String( bytes:data[44..<45], encoding:.utf8)
+        elements[Passport.nationality] = String( bytes:data[45..<48], encoding:.utf8)
+        elements[Passport.mrzLineCheckDigit] = String( bytes:data[59..<60], encoding:.utf8)
+        elements[Passport.firstName] = String( bytes:data[60...], encoding:.utf8)
     }
     
     func parseTd2(_ data : [UInt8]) {
-        elements["5F03"] = String( bytes:data[0..<2], encoding:.utf8)
-        elements["5F28"] = String( bytes:data[2..<5], encoding:.utf8)
-        elements["5B"] = String( bytes:data[5..<36], encoding:.utf8)
-        elements["5A"] = String( bytes:data[36..<45], encoding:.utf8)
-        elements["5F04"] = String( bytes:data[45..<46], encoding:.utf8)
-        elements["5F2C"] = String( bytes:data[46..<49], encoding:.utf8)
-        elements["5F57"] = String( bytes:data[49..<55], encoding:.utf8)
-        elements["5F05"] = String( bytes:data[55..<56], encoding:.utf8)
-        elements["5F35"] = String( bytes:data[56..<57], encoding:.utf8)
-        elements["59"] = String( bytes:data[57..<63], encoding:.utf8)
-        elements["5F06"] = String( bytes:data[63..<64], encoding:.utf8)
-        elements["53"] = String( bytes:data[64..<71], encoding:.utf8)
-        elements["5F07"] = String( bytes:data[71..<72], encoding:.utf8)
+        elements[Passport.documentType] = String( bytes:data[0..<2], encoding:.utf8)
+        elements[Passport.issuingAuthority] = String( bytes:data[2..<5], encoding:.utf8)
+        elements[Passport.firstName] = String( bytes:data[5..<36], encoding:.utf8)
+        elements[Passport.documentNumber] = String( bytes:data[36..<45], encoding:.utf8)
+        elements[Passport.documentNumberCheckDigit] = String( bytes:data[45..<46], encoding:.utf8)
+        elements[Passport.nationality] = String( bytes:data[46..<49], encoding:.utf8)
+        elements[Passport.dateOfBirth] = String( bytes:data[49..<55], encoding:.utf8)
+        elements[Passport.dateOfBirthCheckDigit] = String( bytes:data[55..<56], encoding:.utf8)
+        elements[Passport.gender] = String( bytes:data[56..<57], encoding:.utf8)
+        elements[Passport.documentExpiryDate] = String( bytes:data[57..<63], encoding:.utf8)
+        elements[Passport.documentExpiryDateCheckDigit] = String( bytes:data[63..<64], encoding:.utf8)
+        elements[Passport.personalNumber] = String( bytes:data[64..<71], encoding:.utf8)
+        elements[Passport.mrzLineCheckDigit] = String( bytes:data[71..<72], encoding:.utf8)
     }
     
     func parseOther(_ data : [UInt8]) {
-        elements["5F03"] = String( bytes:data[0..<2], encoding:.utf8)
-        elements["5F28"] = String( bytes:data[2..<5], encoding:.utf8)
-        elements["5B"]   = String( bytes:data[5..<44], encoding:.utf8)
-        elements["5A"]   = String( bytes:data[44..<53], encoding:.utf8)
-        elements["5F04"] = String( bytes:[data[53]], encoding:.utf8)
-        elements["5F2C"] = String( bytes:data[54..<57], encoding:.utf8)
-        elements["5F57"] = String( bytes:data[57..<63], encoding:.utf8)
-        elements["5F05"] = String( bytes:[data[63]], encoding:.utf8)
-        elements["5F35"] = String( bytes:[data[64]], encoding:.utf8)
-        elements["59"]   = String( bytes:data[65..<71], encoding:.utf8)
-        elements["5F06"] = String( bytes:[data[71]], encoding:.utf8)
-        elements["53"]   = String( bytes:data[72..<86], encoding:.utf8)
-        elements["5F02"] = String( bytes:[data[86]], encoding:.utf8)
-        elements["5F07"] = String( bytes:[data[87]], encoding:.utf8)
+        elements[Passport.documentType] = String( bytes:data[0..<2], encoding:.utf8)
+        elements[Passport.issuingAuthority] = String( bytes:data[2..<5], encoding:.utf8)
+        elements[Passport.firstName]   = String( bytes:data[5..<44], encoding:.utf8)
+        elements[Passport.documentNumber]   = String( bytes:data[44..<53], encoding:.utf8)
+        elements[Passport.documentNumberCheckDigit] = String( bytes:[data[53]], encoding:.utf8)
+        elements[Passport.nationality] = String( bytes:data[54..<57], encoding:.utf8)
+        elements[Passport.dateOfBirth] = String( bytes:data[57..<63], encoding:.utf8)
+        elements[Passport.dateOfBirthCheckDigit] = String( bytes:[data[63]], encoding:.utf8)
+        elements[Passport.gender] = String( bytes:[data[64]], encoding:.utf8)
+        elements[Passport.documentExpiryDate]   = String( bytes:data[65..<71], encoding:.utf8)
+        elements[Passport.documentExpiryDateCheckDigit] = String( bytes:[data[71]], encoding:.utf8)
+        elements[Passport.personalNumber]   = String( bytes:data[72..<86], encoding:.utf8)
+        elements[Passport.personalNumberCheckDigit] = String( bytes:[data[86]], encoding:.utf8)
+        elements[Passport.mrzLineCheckDigit] = String( bytes:[data[87]], encoding:.utf8)
     }
     
     private func getMRZType(length: Int) -> DocTypeEnum {
